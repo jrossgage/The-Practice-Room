d3efc429ef1f760d468fe46877e80195fce5f661
Joel <jrossgage@gmail.com>
2021-04-30 16:12:54 -0500

    Worked on practiceNotes. Still needs to have save functionality.

--- src/Views/PracticeView.js
+++ src/Views/PracticeView.js
@@ -10,7 +10,16 @@ import { PracticeNoteCard } from "../components/notes/PracticeNoteCard"
 
 export const PracticeView = ({ sessionId }) => {
 
+    const currentUserId = parseInt(sessionStorage.getItem("app_user_id"))
+
     const [exercises, setExercises] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [note, setNote] = useState({
+        userId: currentUserId,
+        completion_date: 0,
+        progressNote: "",
+        exerciseId: 0
+    })
 
     //What I need: The exercise objects to pass into the PracticeNoteCard.
     //This returns an array of objects with nested exercise objects within each element
@@ -26,6 +35,41 @@ export const PracticeView = ({ sessionId }) => {
             })
     }
 
+    const handleControlledInputChange = (event) => {
+
+        const newNote = { ...note }
+        let selectedVal = event.target.value
+        if (event.target.id.includes("Id")) {
+            selectedVal = parseInt(selectedVal)
+        }
+        newNote[event.target.id.split("--")[0]]= selectedVal
+        setNote(newNote)
+        console.log(selectedVal)
+        console.log(note)
+    }
+
+
+    //When the check box is clicked, I need it to look to the note state. 
+    //IF there are notes that have been written, the exerciseId needs to be taken, the notes need to be saved to the database along with the date that the box is checked.
+    //IF there is no text in the text field, trigger a window alert that tells the user to input notes on progress.
+    const handleCheckboxClick =(event) =>{
+        event.preventDefault()
+        setIsLoading(true)
+
+        const completion_date = note.completion_date
+        const progressNote = note.progressNote 
+        const exerciseId = event.target.id
+
+        if (progressNote === "") {
+            window.alert("Add some notes about your progress")
+         } else {
+            // addNote()
+        }
+
+
+
+    }
+
     useEffect(() => {
         getSessionExercises(sessionId);
     }, [])
@@ -38,6 +82,8 @@ export const PracticeView = ({ sessionId }) => {
                     <PracticeNoteCard
                         key={exercise.id}
                         exercise={exercise}
+                        handleCheckboxClick={handleCheckboxClick}
+                        handleControlledInputChange={handleControlledInputChange}
                     />)}
             </div>
         </>
